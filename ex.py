611b0c1351626e39a11b6958c311c66eb5d5c571
cruiser <33943755+cruiser@users.noreply.github.com>
2024-03-28 09:28:25 -0500

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,64 @@
+import discord
+import asyncio
+import shutil
+import subprocess
+
+client = discord.Client()
+
+token = "" # token for discord
+
+print("* commands *")
+print("- !clear // deletes all messages in channel")
+print("- !clearallmessages // deletes all messages in all DMs")
+
+def owo(cmd):
+    subprocess.call(cmd, shell=True)
+
+@client.event
+async def on_ready():
+
+    width = shutil.get_terminal_size().columns
+
+    def ui():
+        print()
+        print("nuke it".center(width))
+        print()
+        print("[-] Developed by cruiser [-]".center(width))
+        print("[-] User: {0} [-]".format(client.user).center(width))
+        print()
+    ui()
+ 
+ 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        commands = []
+        z = 0
+        for index, a in enumerate(message.content):
+            if a == " ":
+                commands.append(message.content[z:index])
+                z = index + 1
+        commands.append(message.content[z:])
+        channel = message.channel
+
+        if commands[0] == '!clear':
+                    if len(commands) == 1:
+                        async for msg in channel.history(limit=9999):
+                            if msg.author == client.user:
+                                try:
+                                    await msg.delete()
+                                except Exception as x:
+                                    pass
+
+        if commands[0] == '!clearallmessages':
+            for channel in client.private_channels:
+                if isinstance(channel, discord.DMChannel):
+                    async for msg in channel.history(limit=9999):
+                        try:
+                            if msg.author == client.user:
+                                await msg.delete()
+                                print(msg)
+                        except:
+                             pass
+
+client.run(token, bot=False)
